fa53d2652bdc8b4146b1103a0d5e6d4e8f912db3
Gabriel-Almeida-Ajax <58678638+Gabriel-Almeida-Ajax@users.noreply.github.com>
2020-08-09 21:05:33 -0300

    v2

--- lib/src/widgets/cards_Day_Schedule.dart
+++ lib/src/widgets/cards_Day_Schedule.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:language_school/shared/theme/app_colors.dart';
+import 'package:dotted_border/dotted_border.dart';
 
 class cardDaySchedule extends StatefulWidget {
   @override
@@ -18,7 +19,6 @@ class _cardDayScheduleState extends State<cardDaySchedule> {
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(15),
             color: AppColors.yellow,
-            
           ),
           child: Row(
             children: [
@@ -45,8 +45,11 @@ class _cardDayScheduleState extends State<cardDaySchedule> {
                     Container(
                       child: Row(
                         children: [
-                          Icon(Icons.query_builder, color: AppColors.darkYellow),
-                          SizedBox(width: 3,),
+                          Icon(Icons.query_builder,
+                              color: AppColors.darkYellow),
+                          SizedBox(
+                            width: 3,
+                          ),
                           Text(
                             "10:00 - 11:20",
                             style: TextStyle(color: AppColors.darkYellow),
@@ -94,8 +97,10 @@ class _cardDayScheduleState extends State<cardDaySchedule> {
                     Container(
                       child: Row(
                         children: [
-                          Icon(Icons., color: AppColors.darkPurple),
-                          SizedBox(width: 3,),
+                          Icon(Icons.add, color: AppColors.darkPurple),
+                          SizedBox(
+                            width: 3,
+                          ),
                           Text(
                             "11:50 - 12:30",
                             style: TextStyle(color: AppColors.darkPurple),
@@ -115,13 +120,11 @@ class _cardDayScheduleState extends State<cardDaySchedule> {
         ),
         Container(
           decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color: AppColors.green
-          ),
+              borderRadius: BorderRadius.circular(15), color: AppColors.green),
           child: Row(
             children: [
               Container(
-                child: Icon(Icons.cloud, color: AppColors.darkGreen, size: 50),
+                child: Icon(Icons.account_balance, color: AppColors.darkGreen, size: 50),
                 width: 115,
                 height: 175,
               ),
@@ -131,22 +134,24 @@ class _cardDayScheduleState extends State<cardDaySchedule> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      "Weather",
+                      "Culture",
                       style: TextStyle(
                           color: AppColors.darkGreen,
                           fontWeight: FontWeight.bold,
                           fontSize: 15),
                     ),
                     SizedBox(height: 5),
-                    Text("Speaking Practice"),
+                    Text("New Topic"),
                     SizedBox(height: 10),
                     Container(
                       child: Row(
                         children: [
                           Icon(Icons.query_builder, color: AppColors.darkGreen),
-                          SizedBox(width: 3,),
+                          SizedBox(
+                            width: 3,
+                          ),
                           Text(
-                            "10:00 - 11:20",
+                            "13:00 - 13:40",
                             style: TextStyle(color: AppColors.darkGreen),
                           )
                         ],
@@ -170,7 +175,7 @@ class _cardDayScheduleState extends State<cardDaySchedule> {
           child: Row(
             children: [
               Container(
-                child: Icon(Icons.cloud, color: AppColors.darkPink, size: 50),
+                child: Icon(Icons.book, color: AppColors.darkPink, size: 50),
                 width: 115,
                 height: 175,
               ),
@@ -193,7 +198,9 @@ class _cardDayScheduleState extends State<cardDaySchedule> {
                       child: Row(
                         children: [
                           Icon(Icons.query_builder, color: AppColors.darkPink),
-                          SizedBox(width: 3,),
+                          SizedBox(
+                            width: 3,
+                          ),
                           Text(
                             "10:00 - 11:20",
                             style: TextStyle(color: AppColors.darkPink),
@@ -211,54 +218,53 @@ class _cardDayScheduleState extends State<cardDaySchedule> {
           width: 275,
           height: 150,
         ),
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(15),
-            color: Colors.red[50],
-          ),
-          child: Row(
-            children: [
-              Container(
-                child: Icon(Icons.cloud, color: Colors.red[300], size: 50),
-                width: 115,
-                height: 175,
-              ),
-              Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Weather",
-                      style: TextStyle(
-                          color: Colors.red[400],
-                          fontWeight: FontWeight.bold,
-                          fontSize: 15),
+        
+        DottedBorder(
+          borderType: BorderType.RRect,
+          radius: Radius.circular(15),
+          padding: EdgeInsets.all(0),
+          strokeWidth: 0.8,
+          color: Colors.grey,
+          dashPattern: [7],
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+            child: Container(
+              child: Row(
+                children: [
+                  Container(
+                    child: Wrap(
+                      spacing: 16.0, // gap between adjacent chips
+                      runSpacing: 16.0, // gap between lines
+                      children: <Widget>[
+                        SizedBox(
+                          width: 15,
+                        ),
+                        Container(
+                          child: Text(
+                            "Add More Lessons",
+                            style: TextStyle(
+                                fontSize: 25, color: Colors.grey[800]),
+                          ),
+                          width: 120,
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.blue[50],
+                          ),
+                          child: Icon(Icons.add, color: AppColors.darkBlue, size: 35,),
+                          width: 60,
+                          height: 60,
+                        )
+                      ],
                     ),
-                    SizedBox(height: 5),
-                    Text("Speaking Practice"),
-                    SizedBox(height: 10),
-                    Container(
-                      child: Row(
-                        children: [
-                          Icon(Icons.query_builder, color: Colors.red[300]),
-                          SizedBox(width: 3,),
-                          Text(
-                            "10:00 - 11:20",
-                            style: TextStyle(color: Colors.red[300]),
-                          )
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                width: 160,
-                height: 175,
+                  ),
+                ],
               ),
-            ],
+              width: 275,
+              height: 150,
+            ),
           ),
-          width: 275,
-          height: 150,
         ),
       ],
     ));
